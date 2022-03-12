9bd0dcfba892efa79727f2b7f13b722530781d77
cryst91 <christanty.fransiska@gmail.com>
2022-03-12 10:14:33 +0700

    update 12/3 sesi 1

--- src/controllers/classes/routes.js
+++ src/controllers/classes/routes.js
@@ -3,11 +3,22 @@ const router = express.Router();
 const createClass = require("./create.classController");
 const updateClass = require("./update.classController");
 const deleteClass = require("./delete.classController");
+const readClass = require("./read.classController");
+const
+
 
 router.post("/", createClass.service);
 router.put("/:id", updateClass.service);
 router.delete("/:id", deleteClass.service);
+router.get("/", readClass.service);
+router.get("/:id/")
 
 module.exports = router;
 
 
+// {
+//     "name": "Kelas Fullstack Shift Academy",
+//     "description": "kelas fullstack materi backend fundamental",
+//     "dateFrom": "2022-06-01",
+//     "dateTo": "2022-09-30"
+// }
